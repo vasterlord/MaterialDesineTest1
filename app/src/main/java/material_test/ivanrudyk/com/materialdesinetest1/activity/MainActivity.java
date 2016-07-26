5f4369a4747b592c0abc9752d22391f3f96e48f5
IvanRudyk1996 <ivanrudyk1996@gmail.com>
2016-07-26 18:50:02 +0300

    Simple Desine

---
+++ app/src/main/java/material_test/ivanrudyk/com/materialdesinetest1/activity/MainActivity.java
@@ -0,0 +1,66 @@
+package material_test.ivanrudyk.com.materialdesinetest1.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import material_test.ivanrudyk.com.materialdesinetest1.R;
+import material_test.ivanrudyk.com.materialdesinetest1.fragment.NavigationDraverFragment;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        onCreareToolBar();
+    }
+
+
+    private void onCreareToolBar() {
+        toolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        NavigationDraverFragment draverFragment = (NavigationDraverFragment)
+                getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_draver);
+        draverFragment.setUp(R.id.fragment_navigation_draver, (DrawerLayout) findViewById(R.id.drawer_layout), toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        if (id == R.id.navigate) {
+            startActivity(new Intent(this, SubActivity.class));
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
